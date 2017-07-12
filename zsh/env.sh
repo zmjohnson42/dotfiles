3f96d6cb5557aa516d44272e3afc3c31e3f589e4
Zach Johnson <zach.johnson@dialogtech.com>
2017-07-12 09:09:31 -0500

    ADD new Carbon project, zsh alias and gitconfig sym

--- zsh/env.sh
+++ zsh/env.sh
@@ -5,4 +5,13 @@ export PATH="/usr/local/share/python:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbi
 # export EDITOR='subl -w'
 
 # ALIASES
+## folders
+alias p="cd ~/Projects"
+alias .f="cd ~/.dotfiles"
 
+## git
+alias gs="git status -s"
+alias ga="git add -A"
+alias gc="git commit"
+alias gf="git fetch"
+alias gfa="git fetch --all"
