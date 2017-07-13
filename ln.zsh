312f9dafecf7209b0c2c62632cb985a804fe42c1
Zach Johnson <zach.johnson@dialogtech.com>
2017-07-13 09:06:35 -0500

    ADD Sublime preferences link and update ln script

--- ln.zsh
+++ ln.zsh
@@ -6,3 +6,4 @@ ln -s ~/.dotfiles/vim/.vimrc ~
 ln -s ~/.dotfiles/vim/.vim_runtime ~
 
 ln -s ~/.dotfiles/sublime-text/projects ~/Documents/sublime-text/
+ln -s ~/.dotfiles/sublime-text/settings/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
