7965f39ade038018c13dd96d302ec263f1e22d50
Zach Johnson <zach.johnson@dialogtech.com>
2017-12-07 10:37:32 -0600

    ADD vscode workspace

--- ln.zsh
+++ ln.zsh
@@ -7,6 +7,8 @@ ln -s $ROOT/vim/.vim_runtime ~
 ln -s $ROOT/sublime-text/projects ~/Documents/sublime-text/
 ln -s $ROOT/sublime-text/settings/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
+ln -s $ROOT/vscode/CRBN.code-workspace ~/Documents/vscode/
+
 ln -s $ROOT/zsh/.oh-my-zsh ~
 ln -s $ROOT/zsh/.zshrc ~
 ln -s $ROOT/zsh/.zlogin ~
